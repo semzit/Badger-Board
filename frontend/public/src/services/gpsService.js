00764298e915a3b41d16028731f8e512f0fe4aee
semzit <zitouni.skz@gmail.com>
2025-11-22 14:59:25 -0600

    added hooks and gps

---
+++ frontend/public/src/services/gpsService.js
@@ -0,0 +1,18 @@
+const x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function success(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function error() {
+  alert("Sorry, no position available.");
+}
