57fd6146acd8d0564535acf5017bba6043e2c892
semzit <zitouni.skz@gmail.com>
2026-01-07 19:32:40 -0600

    infra not working

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -4,6 +4,7 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import * as efs from 'aws-cdk-lib/aws-efs'
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'; 
 import { Repository } from 'aws-cdk-lib/aws-ecr';
 
 export class InfraStack extends cdk.Stack {
@@ -19,6 +20,12 @@ export class InfraStack extends cdk.Stack {
       ]
     }); 
 
+    // Create load balancer 
+    const loadBalancer = new elbv2.ApplicationLoadBalancer(this, "BadgerBoardALB", {
+      vpc: vpc, 
+      internetFacing: true
+    })
+
     // Create EFS file system and access point 
     const fileSystem = new efs.FileSystem(this, 'BadgerBoards', {
       vpc: vpc,
@@ -28,34 +35,137 @@ export class InfraStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY 
     });
 
-    fileSystem.addAccessPoint("BoardsAccessPoint", {
+    const accessPointEFS = fileSystem.addAccessPoint("BoardsAccessPoint", {
       path: "/board", 
+      createAcl: {
+        ownerUid: "1000",
+        ownerGid: "1000",
+        permissions: "755",
+      },
       posixUser: {
         uid: "1000",
         gid: "1000"
       }
-    })
+    }); 
     
     // ECS cluster
     const cluster = new ecs.Cluster(this, "BadgerBoardCluster",{
       vpc: vpc
-    })
+    });
 
     // backend service
-    const backend = new ecsp.ApplicationMultipleTargetGroupsFargateService(this, "BadgerBoardBackend", {
-      taskImageOptions: {
-        image: ecs.ContainerImage.fromAsset("../../backend"), 
-      } 
-      
+    const backendTask = new ecs.FargateTaskDefinition(this, "BackendTask", {
+      cpu: 256, 
+      memoryLimitMiB: 512, 
     }); 
 
-    // Connect ECS to task
 
-    // Handle port 8081
+    fileSystem.grant(backendTask.taskRole, 
+      'elasticfilesystem:ClientMount', 
+      'elasticfilesystem:ClientWrite',
+      'elasticfilesystem:ClientRootAccess' // Required for mounting via Access Points
+    );
+    
+
+    backendTask.addVolume({
+      name: "BadgerBoards", 
+      efsVolumeConfiguration: {
+        fileSystemId:fileSystem.fileSystemId, 
+        transitEncryption: "ENABLED",
+        authorizationConfig: {
+          accessPointId: accessPointEFS.accessPointId, 
+          iam: "ENABLED"
+        } ,
+      }, 
+    }); 
+
+    const backendContainer = backendTask.addContainer("BackendContainer", {
+      image: ecs.ContainerImage.fromAsset("../backend"),
+      logging: ecs.LogDriver.awsLogs({streamPrefix: "backend"})
+    }); 
+
+    backendContainer.addPortMappings(
+      {containerPort: 8080},
+      {containerPort: 8081}
+    );
+
+    backendContainer.addMountPoints({
+      containerPath: '/data', 
+      sourceVolume: "BadgerBoards", 
+      readOnly: false
+    })
+
+    backendContainer.addEnvironment("DB_PATH" , "Boards.db"); 
+
+    const backendService =  new ecs.FargateService(this, "backendService", {
+      cluster: cluster, 
+      taskDefinition: backendTask, 
+      assignPublicIp: true
+    }); 
+
+    fileSystem.connections.allowDefaultPortFrom(backendService);
+
+    const listener = loadBalancer.addListener("PublicListener", {
+      port: 80, 
+    })
+
+    // API Routing (/api/* -> 8080)
+    listener.addTargets('ApiTarget', {
+      priority: 1,
+      conditions: [elbv2.ListenerCondition.pathPatterns(['/api/*'])],
+      port: 80,
+      targets: [backendService.loadBalancerTarget({
+        containerName: 'BackendContainer',
+        containerPort: 8080,
+      })],
+      healthCheck: { path: '/api/' } 
+    });
+
+    // WS Routing (/ws/* -> 8081)
+    listener.addTargets('WsTarget', {
+      priority: 2,
+      conditions: [elbv2.ListenerCondition.pathPatterns(['/ws/*'])],
+      port: 80,
+      targets: [backendService.loadBalancerTarget({
+        containerName: backendContainer.containerName,
+        containerPort: 8081,
+      })],
+      healthCheck: { 
+        path: '/api/',
+        port : "8080"  // Hard to deal with WS port for health checks 
+      } 
+    });
 
     // Frontend Service 
+    const frontendTask = new ecs.FargateTaskDefinition(this, "FrontendTask", {
+      cpu : 256, 
+      memoryLimitMiB: 512
+    }); 
 
+    const frontendContainer = frontendTask.addContainer("FrontendContainer", {
+      image: ecs.ContainerImage.fromAsset('../frontend'), 
+      logging: ecs.LogDriver.awsLogs({streamPrefix: "backend"})
+    });
 
+    frontendContainer.addPortMappings(
+      {containerPort: 80}
+    ); 
     
+    const frontendService = new ecs.FargateService(this, "FrontendService", {
+      cluster: cluster, 
+      taskDefinition: frontendTask,
+      assignPublicIp: true
+    })
+
+    listener.addTargets("FrontendTargets", {
+      port: 80, 
+      targets: [frontendService.loadBalancerTarget({
+        containerName: "FrontendContainer", 
+        containerPort: 80
+      })],
+      healthCheck: { path: '/' }
+    });
+
+    fileSystem.connections.allowDefaultPortFrom(backendService);
   }
 }
