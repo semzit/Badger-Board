ef70737fff60340dfc4b3e027a5e9ac05b8b92a1
Semo Zitouni <zitouni.skz@gmail.com>
2026-08-16 17:20:11 -0500

    chore: move apps into pnpm workspace

---
+++ apps/api/src/services/geoService.ts
@@ -0,0 +1,12 @@
+import { LatLon, insidePolygon } from "geolocation-utils";
+import { coords } from "../types/types";
+
+/**
+ * Validate coords fit inside of the building
+ * @param cords
+ * @param coords2
+ * @returns
+ */
+export const validCoords = (cords: LatLon, coords2: coords) => {
+  return insidePolygon(cords, coords2);
+};
